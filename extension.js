996b6e55db4c6aa236b8d1ae28c3bfd8ce888b6a
mark <miconico@gmail.com>
2016-02-09 22:37:24 +0000

    weather api

--- extension.js
+++ extension.js
@@ -8,26 +8,18 @@
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp'];
-                  callback(temperature);
-              }
-        });
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
     };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
